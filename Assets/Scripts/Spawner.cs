2612f89801747a32d1d4b2bf53b61c68e435111b
Kam-ay <gokaykayhan@gmail.com>
2023-11-18 14:23:07 +0300

    Sonsuz loop
    Umarım silinmez yaptıklarım

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+
+    public GameObject Box;
+    private GameObject randomSpawnSpot;
+    private int timer = 0;
+    public GameObject[] boxPositonList;
+    public int gerisayimCount = 0;
+    private bool go = true;
+    private Vector3 V;
+    private Quaternion Q = Quaternion.identity;
+    
+        void Start()
+    {
+        Debug.Log(0);
+        StartCoroutine(BoxGen());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator BoxGen()
+    {
+        Debug.Log(1);
+        while (go)
+        {
+            randomSpawnSpot = boxPositonList[Random.Range(0, boxPositonList.Length)];
+            if (randomSpawnSpot.GetComponent<Full>().fullOrNot == false)
+            {
+                V = new Vector3(randomSpawnSpot.transform.position.x, randomSpawnSpot.transform.position.y, randomSpawnSpot.transform.position.z);
+                Instantiate(Box, V,Q) ;
+                new WaitForSeconds(10 - gerisayimCount);
+                Debug.Log(2);
+                go = false;
+                randomSpawnSpot.GetComponent<Full>().fullOrNot = true;
+            }
+
+            for (int i = 0; i < boxPositonList.Length; i++)
+            {
+
+                if (boxPositonList[i].GetComponent<Full>().fullOrNot == false)
+                {
+                    go = true;
+                    Debug.Log(123);
+                    break;
+                }    
+            }
+
+        }
+        yield return new WaitForSeconds(1);
+    }
+}
+
