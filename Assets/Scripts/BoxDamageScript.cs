b84d32d1ab66c3f0d71eda85fad780f112dd75fe
emircihanalper <emircihanalper@gmail.com>
2023-11-19 16:30:25 +0300

    1

---
+++ Assets/Scripts/BoxDamageScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDamageScript : MonoBehaviour
+{
+    public GameObject[] Boxes ;
+    int boxNo;
+    void Start()
+    {
+        boxNo = 0;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TestTag"))
+        {
+            Boxes[boxNo].gameObject.SetActive(false);
+            boxNo++;
+            Boxes[boxNo].gameObject.SetActive(true);
+            if (boxNo == 2)
+            {                
+                Destroy(this.gameObject);
+            }
+        }
+    }    
+}
