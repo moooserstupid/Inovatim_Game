ca37be26af1bfda67f5e4323659458cf832f0013
emircihanalper <emircihanalper@gmail.com>
2023-11-25 10:17:00 +0300

    menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject firstMenu;
+    public GameObject secondMenu;
+    public GameObject pos1;
+    public GameObject pos2;
+    float speed;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        firstMenu.SetActive(false);
+        secondMenu.SetActive(true);
+        speed = 30.0f;
     }
     public void CreditsLevel()
     {
@@ -20,6 +28,20 @@ public class MenuScript : MonoBehaviour
     public void Return()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void LetsRoll()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Backout()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void FixedUpdate()
+    {
+        pos1.transform.position = Vector3.MoveTowards(pos1.transform.position, pos2.transform.position, speed * Time.deltaTime);
     }
 
 }
